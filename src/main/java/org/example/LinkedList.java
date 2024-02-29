00841894a4690aadaa912fd338f095b5d7f0bbfc
Ravi Fonseca <ravi.fonseca8@gmail.com>
2024-02-29 15:24:27 +0100

    added partition algorithm

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -278,4 +278,43 @@ public class LinkedList {
 
         return slow; // Return the kth node from the end (slow pointer)
     }
+
+    public void partitionList (int x){
+        if (head == null) return;
+        Node dummy1 = new Node(0);
+        Node dummy2 = new Node(0);
+        Node prev1 = dummy1;
+        Node prev2 = dummy2;
+        Node current = head;
+
+        while (current != null){
+            if(current.value < x){
+                prev1.next = current;
+                prev1 = current;
+            }
+            else{
+                prev2.next = current;
+                prev2 = current;
+            }
+            current = current.next;
+        }
+
+        prev2.next=null;
+        prev1.next = dummy2.next;
+        head = dummy1.next;
+
+//        Node temp = head;
+//        Node smaller = head;
+//        Node bigger = null;
+//        if (temp == null) return;
+//        while (temp != null){
+//            if(temp.value < x){
+//                smaller.next = temp;
+//            }
+//            else{
+//                bigger.next = temp;
+//            }
+//            temp = temp.next;
+//        }
+    }
 }
